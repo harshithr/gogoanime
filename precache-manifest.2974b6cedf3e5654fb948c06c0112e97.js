2c313d50fdaa7f996f67ab3bd7c5c5405c93f69d
harshithr <harshithr.pro@gmail.com>
2020-11-17 22:50:08 +0530

    Updates

--- precache-manifest.2974b6cedf3e5654fb948c06c0112e97.js
+++ precache-manifest.2974b6cedf3e5654fb948c06c0112e97.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cb342aeda80bf46319860a36a5273731",
+    "revision": "adbe2abf3072b42309c41ec9f19cf562",
     "url": "/gogoanime/index.html"
   },
   {
-    "revision": "d44bc165753d806e29ab",
+    "revision": "fa15920922314fee7fa6",
     "url": "/gogoanime/static/css/main.0d95b0a3.chunk.css"
   },
   {
-    "revision": "216822d43e5af2ca8511",
-    "url": "/gogoanime/static/js/2.b67c4f9e.chunk.js"
+    "revision": "b4ea341ff5f86764a363",
+    "url": "/gogoanime/static/js/2.bde6fd91.chunk.js"
   },
   {
     "revision": "25ee36cecf3742d8e72fa8504c054097",
-    "url": "/gogoanime/static/js/2.b67c4f9e.chunk.js.LICENSE.txt"
+    "url": "/gogoanime/static/js/2.bde6fd91.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d44bc165753d806e29ab",
-    "url": "/gogoanime/static/js/main.86d31b68.chunk.js"
+    "revision": "fa15920922314fee7fa6",
+    "url": "/gogoanime/static/js/main.bef95143.chunk.js"
   },
   {
     "revision": "c9e8817316724b7e15fc",
