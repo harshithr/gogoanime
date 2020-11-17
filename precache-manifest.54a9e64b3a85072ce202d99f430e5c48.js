1b77920c961a8c80505e28058670e6f34cfe052d
harshithr <harshithr.pro@gmail.com>
2020-11-17 22:29:51 +0530

    Updates

---
+++ precache-manifest.54a9e64b3a85072ce202d99f430e5c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb342aeda80bf46319860a36a5273731",
+    "url": "/gogoanime/index.html"
+  },
+  {
+    "revision": "d44bc165753d806e29ab",
+    "url": "/gogoanime/static/css/main.0d95b0a3.chunk.css"
+  },
+  {
+    "revision": "216822d43e5af2ca8511",
+    "url": "/gogoanime/static/js/2.b67c4f9e.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/gogoanime/static/js/2.b67c4f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44bc165753d806e29ab",
+    "url": "/gogoanime/static/js/main.86d31b68.chunk.js"
+  },
+  {
+    "revision": "c9e8817316724b7e15fc",
+    "url": "/gogoanime/static/js/runtime-main.b8678b69.js"
+  }
+]);
